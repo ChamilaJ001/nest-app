057a85d6f523c1a2a63bdc804444dbfcaae74a42
Chamila Jayasinghe <official.chamilaj@gmail.com>
2024-06-09 23:00:53 +0530

    Implemet middleware and exceptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleWare } from 'logger.middleware';
+import { UsersController } from './users/users.controller';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'exceptions/http-exception.filter';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleWare).forRoutes(UsersController);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { HttpExceptionFilter } from 'exceptions/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
